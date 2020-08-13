be51f8202edc46f3c1dc50abd96c150a37be524a
Aashi Rastogi <ARastogi4@slb.com>
2020-08-13 22:46:21 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { SettingsComponent } from './settings/settings.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
+    SettingsComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule
